99cd2c4586a1961534525abeb3b0a923437c4d47
KDAppsDec22 <kaushaldubey71@gmail.com>
2024-11-09 00:47:17 +0530

    create modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
+import { HomeComponent } from './home/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 //  here create all routes navigation
+
+//  command for create files and folder structure
+    //  ng g c login/login
---
+++ src/app/listing/bloglisting/bloglisting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bloglisting',
+  standalone: true,
+  imports: [],
+  templateUrl: './bloglisting.component.html',
+  styleUrl: './bloglisting.component.css'
+})
+export class BloglistingComponent {
+
+}
